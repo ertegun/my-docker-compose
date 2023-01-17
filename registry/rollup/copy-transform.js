1187af87837947cf65b52d5985dfc04af7c337ac
ertegun <ertegun@users.noreply.github.com>
2023-01-17 10:49:51 +0300

    joxit-registry

--- registry/rollup/copy-transform.js
+++
@@ -1,6 +0,0 @@
-export default function (contents, name) {
-  if (name.endsWith('.svg')) {
-    return contents.toString('utf8').split(/\n */).join(' ').replace(/\s+/g, ' ').trim();
-  }
-  return contents;
-}
