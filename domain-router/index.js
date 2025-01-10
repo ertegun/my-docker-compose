cfec7b8a2c0512a02e60669ea93c94cc92f5e5fd
ertegun <ertegun@users.noreply.github.com>
2025-01-10 09:51:12 +0300

    domain-router updated

--- domain-router/index.js
+++ domain-router/index.js
@@ -16,36 +16,41 @@ app.get("/", (req, res) => {
   res.send("Hoş geldiniz!");
 });
 
-app.get("/:lang/:device_code", (req, res) => {
-  const { lang } = req.params;
-  const { device_code } = req.params;
-  let address = "https://www.gruparge.com";
+app.get("/:p1/:p2", (req, res) => {
+  const { p1, p2 } = req.params;
+  let address = `https://www.gruparge.com/${p1}/${p2}/`;
 
-  switch (device_code) {
-    case "rkrs":
-      switch (lang) {
-        case "en":
-          address = "https://www.gruparge.com/wp-content/uploads/2024/04/power-factor-controller-RKRS-user-manuel.pdf";
-          break;
-        default: //tr
-          address = "https://www.gruparge.com/wp-content/uploads/2023/05/reaktif-kontrol-rolesi-rkrs-kullanma-kilavuzu.pdf";
-          break;
-      }
-      break;
-    case "rkr":
-      switch (lang) {
-        case "en":
-          address = "https://www.gruparge.com/wp-content/uploads/2023/01/reagent-control-relay-rkr-user-manual.pdf";
-          break;
-        default: //tr
-          address = "https://www.gruparge.com/wp-content/uploads/2023/01/rkr-serisi-reaktif-guc-kontrol-rolesi-kullanim-kilavuzu.pdf";
-          break;
-      }
-      break;
-    default:
-      address = address;
-      break;
-  }
+  // switch (p2) {
+  //   case "rkrs":
+  //     switch (p1) {
+  //       case "en":
+  //         address = "https://www.gruparge.com/wp-content/uploads/2024/04/power-factor-controller-RKRS-user-manuel.pdf";
+  //         break;
+  //       default: //tr
+  //         address = "https://www.gruparge.com/wp-content/uploads/2023/05/reaktif-kontrol-rolesi-rkrs-kullanma-kilavuzu.pdf";
+  //         break;
+  //     }
+  //     break;
+  //   case "rkr":
+  //     switch (p1) {
+  //       case "en":
+  //         address = "https://www.gruparge.com/wp-content/uploads/2023/01/reagent-control-relay-rkr-user-manual.pdf";
+  //         break;
+  //       default: //tr
+  //         address = "https://www.gruparge.com/wp-content/uploads/2023/01/rkr-serisi-reaktif-guc-kontrol-rolesi-kullanim-kilavuzu.pdf";
+  //         break;
+  //     }
+  //     break;
+  //   default:
+  //     address = address;
+  //     break;
+  // }
+  res.redirect(301, address);
+});
+
+app.get("/:p3", (req, res) => {
+  const { p3 } = req.params;
+  let address = `https://www.gruparge.com/${p3}`;
   res.redirect(301, address);
 });
 
